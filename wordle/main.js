0c8aece656b0bacb4adfac545a02180e892d7b1a
CjPhoenix <83119836+CjPhoenix@users.noreply.github.com>
2022-02-19 15:27:28 -0600

    Create main.js

---
+++ wordle/main.js
@@ -0,0 +1,16 @@
+switch (documents.domain) {
+    case 'www.nytimes.com':
+        execute('nyt');
+        
+    case 'rbrignall.github.io':
+    case 'www.byrdle.net':
+        execute('byrdle');
+        
+    case 'nerdlegame.com':
+        key_stats = 'statsState'
+        key_state = 'gameState'
+}
+
+function execute(name) {
+  fetch("https://raw.githubusercontent.com/CjPhoenix/bookmarklets/main/wordle/" + name + ".js").then((res) => res.text().then((t) => eval(t)))
+}
